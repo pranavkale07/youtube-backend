79f644f02f016731464f14c4f74d2437e7b4b201
pranavkale07 <pranavkale911@gmail.com>
2024-11-24 18:25:22 +0530

    fixed minor bugs in controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -261,7 +261,13 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
     return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully")
+    .json(
+        new ApiResponse(
+            200,
+            req.user,
+            "current user fetched successfully"
+        )
+    )
 })
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
@@ -280,7 +286,7 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
             }
         },
         {new: true}
-    ).select("-password")
+    ).select("-password -refreshToken")
 
     return res
     .status(200)
@@ -309,7 +315,7 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
             }
         },
         {new: true}
-    ).select("-password")
+    ).select("-password -refreshToken")
 
     return res
     .status(200)
